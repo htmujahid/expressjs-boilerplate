c4a6e86f8d62ed6609f947bf14f4ed446d5cadd0
htmujahid <htmujahid@gmail.com>
2023-03-13 02:47:29 +0500

    initial commit

--- src/bootstrap/routes.js
+++ src/bootstrap/routes.js
@@ -1,15 +1,11 @@
-import fs from "fs";
-import path from "path";
+import fs from 'fs';
+import path from 'path';
 
 export async function loadRoutes(app) {
-    fs.readdir(path.join("src","routes"), (err, files) => {
+    fs.readdir(path.join('src', 'routes'), (err, files) => {
         if (err) {
             console.error(err);
         }
-        files.forEach((file) => 
-            import(`../routes/${file}`).then((route) =>
-                app.use("/api", route.default)
-            )
-        );
+        files.forEach(file => import(`../routes/${file}`).then(route => app.use('/api', route.default)));
     });
 }
